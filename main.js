348d2baad98ee01dd39c1f8fc93cfbc8b946b5ab
André Jaques Batista <andrejaques.b@gmail.com>
2021-02-18 16:34:12 -0300

    half way to hamburg menu

---
+++ main.js
@@ -0,0 +1,35 @@
+const sandubaSideMenu = document.querySelector('.G1Z');
+const sandubaMobMenu = document.querySelector('.G3Z');
+let checkBox1 = document.querySelector('#checkbox-menu1:checked');
+let checkBox2 = document.querySelector('#checkbox-menu2:checked');
+
+//Sanduich Icon Menu Side Bar (G1Z)
+function menuSideFunction()
+{
+    console.log(checkBox1)
+}
+/*     if (checkBox1 = true)
+    {
+        sandubaSideMenu.style.display = "flex";
+    }
+    else
+    {
+        sandubaSideMenu.style.display = "none";
+    }
+}
+
+//Sanduich Icon Menu Top Bar (G3Z)
+function menuMobFunction()
+{
+
+    if (checkBox2.checked = true)
+    {
+        sandubaMobMenu.display = "flex";
+    }
+    else
+    {
+        sandubaMobMenu.display = "none";
+    }
+} */
+
+// refazer com o add event listener
